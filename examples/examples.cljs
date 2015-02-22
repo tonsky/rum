ccd417523750f1ce6d6156ee21dfbbe1089a975f
Nikita Prokopov <prokopov@gmail.com>
2015-02-23 03:19:01 +0600

    form validation example

--- examples/examples.cljs
+++ examples/examples.cljs
@@ -231,7 +231,7 @@
 (rum/defc art-rboard []
   [:div.artboard
     (for [y (range 0 board-height)]
-      [:div.art-row
+      [:div.art-row {:key y}
         (for [x (range 0 board-width)]
           ;; this is how one can specify React key for component
           (rum/with-props rcell x y :rum/key [x y]))])
@@ -261,11 +261,62 @@
   [:div.artboard
     (for [y (range 0 board-height)
           :let [y-cursor (rum/cursor board [y])]]
-      [:div.art-row
+      [:div.art-row {:key y}
         (for [x (range 0 board-width)
               :let [x-cursor (rum/cursor y-cursor [x])]]
-          (art-cell x y x-cursor))])
+          (rum/with-props art-cell x y x-cursor :rum/key [x y]))])
     (board-stats board board-renders)])
 
 (defn ^:export start-artboard [mount-el]
   (rum/mount (artboard board) mount-el))
+
+
+;; Form validation
+
+(rum/defc validating-input < rum/reactive [ref fn]
+  [:input {:type "text"
+           :style {:width 170
+                   :background-color (when-not (fn (rum/react ref))
+                                       (rum/react color))}
+           :value (rum/react ref)
+           :on-change #(reset! ref (.. % -target -value))}])
+
+(rum/defcs restricting-input < rum/reactive [state ref fn]
+  (let [comp (:rum/react-component state)]
+    [:input {:type "text"
+             :style {:width 170}
+             :value (rum/react ref)
+             :on-change #(let [new-val (.. % -target -value)]
+                           (if (fn new-val)
+                             (reset! ref new-val)
+                             ;; request-render is mandatory because sablono :input
+                             ;; keeps current value in input’s state and always applies changes to it
+                             (rum/request-render comp)))}]))
+
+(rum/defcs restricting-input-native < rum/reactive [state ref fn]
+  (let [comp (:rum/react-component state)]
+    (js/React.DOM.input
+      #js {:type "text"
+           :style #js {:width 170}
+           :value (rum/react ref)
+           :onChange #(let [new-val (.. % -target -value)]
+                         (when (fn new-val)
+                           (reset! ref new-val))
+                         ;; need forceUpdate here because otherwise rendering will be delayed until requestAnimationFrame 
+                         ;; and that breaks cursor position inside input
+                         (.forceUpdate comp))})))
+
+(rum/defc val-form []
+  (let [state (atom {:email "a@b.c"
+                     :phone "+7913 000 0000"
+                     :age   "22"})]
+    [:dl
+      [:dt "E-mail:"]
+      [:dd (validating-input  (rum/cursor state [:email]) #(re-matches #"[^@]+@[^@.]+\..+" %))]
+      [:dt "Phone:"]
+      [:dd (restricting-input (rum/cursor state [:phone]) #(re-matches #"[0-9\- +()]*" %))]
+      [:dt "Age:"]
+      [:dd (restricting-input-native (rum/cursor state [:age]) #(re-matches #"([1-9][0-9]*)?" %))]]))
+
+(defn ^:export start-val-form [mount-el]
+  (rum/mount (val-form) mount-el))
