646ee73a6ebe5561eabf6e64532b34df3b0dd85f
Roman Liutikov <roman01la@romanliutikov.com>
2018-07-02 13:16:20 +0300

    update React Context example

--- examples/rum/examples/context.cljs
+++ examples/rum/examples/context.cljs
@@ -1,33 +1,31 @@
 (ns rum.examples.context
   (:require
     [rum.core :as rum]
-    [rum.examples.core :as core]
-    [cljsjs.prop-types]))
+    [sablono.core :refer [html]]
+    [rum.examples.core :as core]))
 
 ;; Components with context that all descendants have access to implicitly.
 
-;; This is useful when you are using child components you cannot modify. 
-;; For example, a JS library that gives you components which rely on a context
-;; value being set by an ancestor component.
+;; https://reactjs.org/docs/context.html
+;; Context is designed to share data that can be considered “global”
+;; for a tree of React components,
+;; such as the current authenticated user, theme, or preferred language.
 
-
-(rum/defcc rum-context-comp < { :static-properties { :contextTypes {:color js/PropTypes.string}}}
-  [comp]
-  [:span
-    { :style { :color (.. comp -context -color) }}
-    "Child component uses context to set font color."])
-
-
-;; Assume the following component is from our source code.
 (def color-theme
-  { :child-context (fn [state] {:color @core/*color}) 
-    :static-properties { :childContextTypes {:color js/PropTypes.string} } })
+  (rum/create-context @core/*color))
 
 
-(rum/defc context < color-theme []
+(rum/defc rum-context-comp
+  []
+  (rum/with-context color-theme
+    #(html [:span
+            { :style { :color % }}
+            "Child component uses context to set font color."])))
+
+(rum/defc context < rum/reactive []
   [:div
     [:div "Root component implicitly passes data to descendants."]
-    (rum-context-comp)])
+    (rum/provide-context color-theme (rum/react core/*color) (rum-context-comp))])
 
 
 (defn mount! [mount-el]
