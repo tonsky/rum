27648462080c933a400776bde5c13ee4c17ce0eb
roman01la <roman01la@romanliutikov.com>
2020-04-18 15:49:16 +0300

    0.12.0-SNAPSHOT

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject rum "0.11.5-SNAPSHOT"
+(defproject rum "0.12.0-SNAPSHOT"
   :description "ClojureScript wrapper for React"
   :license {:name "Eclipse"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
