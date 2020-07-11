bb26354f5a5b3fa352a90bb87cf1515561a245a7
roman01la <roman01la@romanliutikov.com>
2020-07-12 01:21:04 +0300

    rebuild example page

--- examples/rum/examples_page.clj
+++ examples/rum/examples_page.clj
@@ -17,134 +17,132 @@
    [rum.examples.errors          :as errors]
    [rum.examples.js-components   :as js-components]))
 
-(def page (str
-           "<!doctype html>
-<html>
-  <head>
-    <meta http-equiv='content-type' content='text/html;charset=UTF-8'/>
-    <title>Rum test page</title>
-    <link href='http://cloud.webtype.com/css/34a9dbc8-2766-4967-a61f-35675306f239.css' rel='stylesheet' type='text/css' />
-    <style>
-      * { box-sizing: border-box; vertical-align: top; text-rendering: optimizelegibility; }
-      tr, td, table, tbody { padding: 0; margin: 0; border-collapse: collapse; }
+(defn ->str [v]
+  (cond
+    (keyword? v) (name v)
+    (number? v) (str v "px")
+    :else v))
 
-      body, input, button { font: 16px/20px 'Input Sans Narr', 'Input Sans Narrow', sans-serif;}
+(defn css [styles]
+  (reduce
+    (fn [ret [selector styles]]
+      (str ret
+           (->> styles
+                (reduce-kv #(str %1 (name %2) ":" (->str %3) ";") "{")
+                (str (name selector)))
+           "}"))
+    ""
+    styles))
 
-      .example { border: 2px solid #ccc; border-radius: 2px; padding: 20px; width: 300px; display: inline-block; height: 260px; margin: 10px 5px; line-height: 28px; }
-      .example-title { border-bottom: 1px solid #ccc; margin: -4px 0 20px; line-height: 30px; }
-      dt { width: 82px; float: left; }
-      dt, dd { vertical-align: bottom; line-height: 36px }
-      input { padding: 6px 6px 2px; border: 1px solid #CCC; }
-      input:focus { outline: 2px solid #a3ccf7; }
+(defn style [& v]
+  [:style {:dangerouslySetInnerHTML {:__html (css v)}}])
 
-      .bclock { margin: -6px 0 0 -4px; }
-      .bclock td, .bclock th { height: 25px; font-size: 12px; font-weight: normal; }
-      .bclock th { width: 10px; }
-      .bclock td { width: 25px; border: 4px solid white;  }
-      .bclock-bit { background-color: #EEE; }
-      .bclock .stats { text-align: left; padding-left: 8px; }
+(defn head []
+  [:head
+   [:meta {:http-equiv "content-type" :content "text/html;charset=UTF-8"}]
+   [:title "Rum test page"]
+   [:link {:href "https://fonts.googleapis.com/css2?family=Fira+Code:wght@300;400;500;600;700&display=swap"
+           :ref "stylesheet"}]
+   (style
+     [:* {:box-sizing :border-box
+          :vertical-align :top
+          :text-rendering :optimizelegibility}]
+     ["tr, td, table, tbody" {:padding 0
+                              :margin 0
+                              :border-collapse :collapse}]
+     ["body, input, button" {:font "16px/20px 'Fira Code', sans-serif"}]
+     [:.example {:border "2px solid #ccc"
+                 :border-radius 2
+                 :padding 20
+                 :width 300
+                 :display :inline-block
+                 :height 260
+                 :margin "10px 5px"
+                 :line-height "28px"}]
+     [:.example-title {:border-bottom "1px solid #ccc"
+                       :margin "-4px 0 20px"
+                       :line-height "30px"}]
+     [:dt {:width 130
+           :float :left}]
+     ["dt, dd" {:vertical-align :bottom
+                :line-height "36px"}]
+     [:input {:padding "6px 6px 2px"
+              :border "1px solid #CCC"}]
+     [:input:focus {:outline "2px solid #a3ccf7"}]
+     [:.bclock {:margin "-6px 0 0 -4px"}]
+     [".bclock td, .bclock th" {:height 25
+                                :font-size "12px"
+                                :font-weight :normal}]
+     [".bclock th" {:width 10}]
+     [".bclock td" {:width 25
+                    :border "4px solid white"}]
+     [:.bclock-bit {:background-color "#eee"}]
+     [".bclock .stats" {:text-align :left
+                        :padding-left 8}]
+     [:.artboard {:-webkit-user-select :none
+                  :line-height "10px"}]
+     [:.art-cell {:width 12
+                  :height 12
+                  :margin "0 1px 1px 0"
+                  :display :inline-block
+                  :background-color "#EEE"}]
+     [".artboard .stats" {:font-size "12px"
+                          :line-height "14px"
+                          :margin-top 8}])])
 
-      .artboard { -webkit-user-select: none; line-height: 10px; }
-      .art-cell { width: 12px; height: 12px; margin: 0 1px 1px 0; display: inline-block; background-color: #EEE; }
-      .artboard .stats { font-size: 12px; line-height: 14px; margin-top: 8px; }
-    </style>
-  </head>
+(defn example [title & children]
+  [:.example
+   [:.example-title title]
+   children])
 
-  <body>
+(defn as-html [form]
+  {:dangerouslySetInnerHTML {:__html (rum/render-html form)}})
 
-    <div class=example>
-      <div class=example-title>Timers</div>
-      <div id=timer-static>"   (rum/render-html (timer-static/timer-static "Static" @core/*clock)) "</div>
-      <div id=timer-reactive>" (rum/render-html (timer-reactive/timer-reactive)) "</div>
-    </div>
-
-    <div class=example>
-      <div class=example-title>Controls</div>
-      <div id=controls>" (rum/render-html (controls/controls)) "</div>
-    </div>
-
-    <div class=example>
-      <div class=example-title>Reactive binary clock</div>
-      <div id=binary-clock>" (rum/render-html (binary-clock/binary-clock)) "</div>
-    </div>
-
-    <div class=example>
-      <div class=example-title>Reactive artboard</div>
-      <div id=board-reactive>" (rum/render-html (board-reactive/board-reactive)) "</div>
-    </div>
-
-    <div class=example>
-      <div class=example-title>BMI Calculator</div>
-      <div id=bmi-calculator>" (rum/render-html (bmi-calculator/bmi-calculator)) "</div>
-    </div>
-    
-    <div class=example>
-      <div class=example-title>Form validation</div>
-      <div id=form-validation></div>
-    </div>
-
-    <div class=example>
-      <div class=example-title>Inputs</div>
-      <div id=inputs>" (rum/render-html (inputs/inputs)) "</div>
-    </div>
-            
-    <div class=example>
-      <div class=example-title>Refs</div>
-      <div id=refs>" (rum/render-html (refs/refs)) "</div>
-    </div>
-
-    <div class=example>
-      <div class=example-title>Local state</div>
-      <div id=local-state>" (rum/render-html (local-state/local-state "Clicks count")) "</div>
-    </div>
-            
-    <div class=example>
-      <div class=example-title>Keys</div>
-      <div id=keys>" (rum/render-html (keys/keys)) "</div>
-    </div>
-    
-    <div class=example>
-      <div class=example-title>Self-reference</div>
-      <div id=self-reference>" (rum/render-html (self-reference/self-reference [:a [:b [:c :d [:e] :g]]])) "</div>
-    </div>
-
-    <div class=example>
-      <div class=example-title>Contexts</div>
-      <div id=context></div>
-    </div>
-
-    <div class=example>
-      <div class=example-title>Custom Methods and Data</div>
-      <div id=custom-props></div>
-    </div>
-
-    <div class=example> 
-      <div class=example-title>Multiple Return</div> 
-      <div id=multiple-return>" (rum/render-html (multiple-return/ulist (multiple-return/multiple-return))) "</div>
-    </div> 
-
-    <div class=example> 
-      <div class=example-title>Portals</div> 
-      <div id=portal-off-root></div>
-      <div id=portal-root></div>
-    </div>
-
-    <div class=example> 
-      <div class=example-title>Error boundaries</div> 
-      <p>Server: <span id=server-errors>" (rum/render-html (errors/errors)) "</span></p>
-      <p>Client: <span id=client-errors></span></p>
-    </div>
-
-    <div class=example>
-      <div class=example-title>JavaScript components</div>
-      <div id=js-components>" (rum/render-html (js-components/mount! nil)) "</div>
-    </div>
-
-    <script src='target/main.js' type='text/javascript'></script>
-  </body>
-</html>"))
+(defn page []
+  [:html
+   (head)
+   [:body
+    (example "Timers"
+      [:#timer-static (as-html (timer-static/timer-static "Static" @core/*clock))]
+      [:#timer-reactive (as-html (timer-reactive/timer-reactive))])
+    (example "Controls"
+      [:#controls (as-html (controls/controls))])
+    (example "Reactive binary clock"
+      [:#binary-clock (as-html (binary-clock/binary-clock))])
+    (example "Reactive artboard"
+      [:#board-reactive (as-html (board-reactive/board-reactive))])
+    (example "BMI Calculator"
+      [:#bmi-calculator (as-html (bmi-calculator/bmi-calculator))])
+    (example "Form validation"
+      [:#form-validation])
+    (example "Inputs"
+      [:#inputs (as-html (inputs/inputs))])
+    (example "Refs"
+      [:#refs (as-html (refs/refs))])
+    (example "Local state"
+      [:#local-state (as-html (local-state/local-state "Clicks count"))])
+    (example "Keys"
+      [:#keys (as-html (keys/keys))])
+    (example "Self-reference"
+      [:#self-reference (as-html (self-reference/self-reference [:a [:b [:c :d [:e] :g]]]))])
+    (example "Contexts"
+      [:#context])
+    (example "Custom Methods and Data"
+      [:#custom-props])
+    (example "Multiple Return"
+      [:#multiple-return (as-html (multiple-return/ulist (multiple-return/multiple-return)))])
+    (example "Portals"
+      [:#portal-off-root]
+      [:#portal-root])
+    (example "Error boundaries"
+      [:p "Server: " [:span#server-errors (as-html (errors/errors))]]
+      [:p "Client: " [:span#client-errors]])
+    (example "JavaScript components"
+      [:#js-components (as-html (js-components/mount! nil))])
+    [:script {:src "target/main.js"}]]])
 
 (defn -main [& args]
   (println "Writing \"index.html\"")
-  (spit "index.html" page))
+  (->> (rum/render-static-markup (page))
+       (spit "index.html")))
 
