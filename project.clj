f188f25898db06b3b22720259ec5df21f95032e3
roman01la <roman01la@romanliutikov.com>
2021-09-22 20:26:01 +0300

    0.12.8-SNAPSHOT

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject rum "0.12.7"
+(defproject rum "0.12.8-SNAPSHOT"
   :description "ClojureScript wrapper for React"
   :license {:name "Eclipse"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
